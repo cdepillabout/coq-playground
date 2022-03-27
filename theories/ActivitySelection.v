44c822ed1483bf6b106225cf7a556d135da67914
Dennis Gosnell <cdep.illabout@gmail.com>
2022-03-27 22:01:22 +0900

    update

--- theories/ActivitySelection.v
+++ theories/ActivitySelection.v
@@ -89,3 +89,105 @@ Definition selectCompatibleActivities (l : list Activity) : list Activity :=
   selectCompatibleActivities' 0 l.
   
 Compute selectCompatibleActivities example_activities.
+
+
+
+Print list_ind.
+
+Definition list_ind2 : forall (A : Type) (P : list A -> Prop),
+    P [] ->
+    (forall a, P [a]) ->
+    (forall a b l, P l -> P (a :: b :: l)) ->
+    forall l, P l :=
+  fun _ _ Hnil Hsing H =>
+    fix F l :=
+      match l with
+      | [] => Hnil
+      | [a] => Hsing a
+      | (a :: b :: ts) => H a b ts (F ts)
+      end.
+
+Theorem all_compatible_in_orig_list : forall l a, In a (selectCompatibleActivities l) -> In a l.
+Proof.
+  (*
+  unfold selectCompatibleActivities.
+  induction l.
+  - simpl. auto.
+  - simpl. intros a0 H.
+    destruct H.
+    + left. auto.
+    + simpl in H.
+  *)
+  (*
+  intros l a H.
+  remember (selectCompatibleActivities l) as c.
+  destruct (selectCompatibleActivities l).
+  - simpl in H. subst. destruct H.
+  - subst. simpl in H. destruct H.
+    +  
+  *)
+  (* intros l. induction l using list_ind2.
+  - auto.
+  - simpl. auto.
+  - simpl. intros. destruct H.
+    + auto.
+    + destruct (finish a <=? start b) eqn:E.
+      * *)
+  
+
+Theorem selectCompatibleActivitiesSorted :
+  forall l, StronglySorted FinishBE l -> StronglySorted FinishBE (selectCompatibleActivities l).
+Proof.
+  (*
+  intros l H. induction H; subst.
+  - unfold selectCompatibleActivities. simpl. constructor.
+  - unfold selectCompatibleActivities in *. simpl.
+    apply SSorted_cons.
+    * inversion IHStronglySorted.
+      + admit.
+      + subst. unfold selectCompatibleActivities'.
+    * *)
+  (*
+  intros l. induction l.
+  - unfold selectCompatibleActivities. simpl. auto.
+  - unfold selectCompatibleActivities in *. simpl in *.
+    intro H. constructor.
+    * inversion H; subst. clear H. apply IHl in H2.
+      unfold selectCompatibleActivities'.
+      destruct l.
+      + constructor.
+      + simpl. fold selectCompatibleActivities'.
+  *)
+  (*
+  unfold selectCompatibleActivities.
+  unfold selectCompatibleActivities'.
+  induction l.
+  - auto.
+  - assert (forall n, 0 <=? n = true) by (destruct n; auto).
+    rewrite H. clear H. fold selectCompatibleActivities' in *.
+    intro H.
+    inversion H; subst. clear H. specialize (IHl H2).
+    constructor.
+    induction l.
+    + admit.
+    + unfold selectCompatibleActivities'.
+  *)
+  (*
+  unfold selectCompatibleActivities.
+  intros l.
+  induction l using list_ind2.
+  - simpl. auto.
+  - simpl. auto.
+  - simpl. intros H.
+    inversion H; subst. inversion H3; subst.
+    unfold FinishBE in H4.
+    inversion H2; subst.
+    specialize (IHl H6).
+  *)
+  unfold selectCompatibleActivities.
+  intros l H. induction H; subst.
+  - simpl. constructor. 
+  - simpl. constructor.
+    + inversion IHStronglySorted; subst.
+      * admit.
+      *  
